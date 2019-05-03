97e2a7791683f6c0e4ed1a0533efba275edab3b7
CalieR <calielrushton@gmail.com>
2019-05-03 16:30:21 +0100

    fetch works

--- app/controllers/api/v1/compliments_controller.rb
+++ app/controllers/api/v1/compliments_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ComplimentsController < ApplicationController
 
     def index
         compliment = Compliment.all.sample.content
-        render json: compliment
+        render json: {content: compliment}
     end
   
 end
